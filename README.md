672433c7752370616fd2734366852c42ddf3602e
Sadi Elaskar <selaskar@cliexa.com>
2025-10-02 19:23:34 +0300

    Minor improvements in summary view.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SmartAccountant.Client
+
+## Emulator notes
+
+Make sure to enable Windows Hypervisor Platform
+
+adb reverse tcp:7130 tcp:7130
--- SmartAccountant.Maui/Converters/CurrencyConverter.cs
+++ SmartAccountant.Maui/Converters/CurrencyConverter.cs
@@ -24,3 +24,31 @@ public class CurrencyConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+
+
+public class MonetaryValueConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not MonetaryValue monetaryValue)
+            return null;
+
+        string amountString = monetaryValue.Amount.ToString("F2");
+
+        string? currencySymbol = monetaryValue.Currency switch
+        {
+            Currency.USD => "$",
+            Currency.EUR => "€",
+            Currency.TRY => "₺",
+            _ => null
+        };
+
+        return currencySymbol != null ? $"{currencySymbol} {amountString}"
+            : $"{amountString} {monetaryValue.Currency}";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
