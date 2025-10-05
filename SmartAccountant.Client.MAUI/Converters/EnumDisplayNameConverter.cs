19afda2dd315cdeb0f5197d9bb3aa053048791a8
Sadi Elaskar <selaskar@gmail.com>
2025-10-06 02:44:00 +0300

    Added expenses breakdown to summary page. Added interest and fees as basic summary item.

---
+++ SmartAccountant.Client.MAUI/Converters/EnumDisplayNameConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Reflection;
+
+namespace SmartAccountant.Client.MAUI.Converters;
+
+internal class EnumDisplayNameConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value?.GetType().IsEnum != true
+            || value.ToString() == null)
+            return null;
+
+        FieldInfo? fi = value.GetType().GetField(value.ToString()!);
+        DisplayAttribute? attribute = fi?.GetCustomAttribute<DisplayAttribute>();
+
+        return attribute?.Name ?? value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
